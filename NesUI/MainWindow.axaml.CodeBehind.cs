b2835fe4d40b7b2ec9e7264c55f733717b3088e7
Antonio Neto <pronetao@gmail.com>
2025-11-08 09:06:01 -0300

    Finish

--- NesUI/MainWindow.axaml.CodeBehind.cs
+++ NesUI/MainWindow.axaml.CodeBehind.cs
@@ -1,81 +1,145 @@
-using System;
-using Avalonia;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
 using Avalonia.Controls;
-using Avalonia.Media.Imaging;
-using Avalonia.Platform;
-using CustomTypes;
+using Avalonia.Controls.Documents;
+using Avalonia.Media;
+using Avalonia.Threading;
+using Emulator.Components;
+using Emulator.Components.Enums;
 using Emulator.Components.Interfaces;
+using Emulator.Debug;
+using NesUI.Helpers;
 
 namespace NesUI
 {
-    public enum ScreenSelection
-    {
-        EmulatorScreen,
-        PatternTable1,
-        PatternTable2
-    }
-
-    public class ScreenBinding
-    {
-        private WriteableBitmap _writeableBitmap;
-        private Image _imageControl;
-        private ScreenSelection _screenSelection;
-
-        public ScreenBinding(ScreenSelection screenSelection, Image imageControl, int width, int height)
-        {
-            _writeableBitmap = new WriteableBitmap(PixelSize.FromSize(new Size(width, height), 1), new Vector(96, 96), PixelFormat.Bgra8888, AlphaFormat.Opaque);
-            _screenSelection = screenSelection;
-            _imageControl = imageControl;
-        }
-
-        public WriteableBitmap WriteableBitmap => _writeableBitmap;
-        public Image ImageControl => _imageControl;
-        public ScreenSelection ScreenSelection => _screenSelection;
-
-        public void OnUpdate(IPpu ppu)
-        {
-            var baseBitmap = _screenSelection switch
-            {
-                ScreenSelection.EmulatorScreen => ppu.GetScreen(),
-                ScreenSelection.PatternTable1 => ppu.GetPatternTable(0, 1),
-                ScreenSelection.PatternTable2 => ppu.GetPatternTable(1, 1),
-                _ => throw new ArgumentOutOfRangeException()
-            };
-            var writeableBitmap = UpdateBitmap(_writeableBitmap, baseBitmap);
-            _imageControl.Source = writeableBitmap;
-            _imageControl.InvalidateVisual();
-        }
-        
-        private unsafe static WriteableBitmap UpdateBitmap(WriteableBitmap bitmap, BaseBitmap baseBitmap)
-        {
-            using ILockedFramebuffer lockedFramebuffer = bitmap.Lock();
-
-            void* backBuffer = (void*)lockedFramebuffer.Address;
-            Span<byte> buffer = new Span<byte>(backBuffer, baseBitmap.Width * baseBitmap.Height * 4);
-            var img = baseBitmap.ToBgraFormat();
-
-            for (int i = 0; i < buffer.Length; i++)
-            {
-                buffer[i] = img[i];
-            }
-            return bitmap;
-        }
-    }
     public partial class MainWindow : Window
     {
-        private unsafe static WriteableBitmap UpdateBitmap(WriteableBitmap bitmap, BaseBitmap baseBitmap)
+        private Bus? _bus = null!;
+        private DispatcherTimer _timer = new DispatcherTimer();
+        private List<ScreenBinding> _emulatorScreenBindings;
+        private bool _showMemory = false;
+
+        [MemberNotNull(nameof(_emulatorScreenBindings))]
+        private void InitializeEmulatorWindow()
         {
-            using ILockedFramebuffer lockedFramebuffer = bitmap.Lock();
-
-            void* backBuffer = (void*)lockedFramebuffer.Address;
-            Span<byte> buffer = new Span<byte>(backBuffer, baseBitmap.Width * baseBitmap.Height * 4);
-            var img = baseBitmap.ToBgraFormat();
-
-            for (int i = 0; i < buffer.Length; i++)
+            var emulatorScreen = new ScreenBinding(ScreenSelection.EmulatorScreen, EmulatorWindow, 256, 240);
+            var patternTable0 = new ScreenBinding(ScreenSelection.PatternTable1, PatternTable0, 128, 128);
+            var patternTable1 = new ScreenBinding(ScreenSelection.PatternTable2, PatternTable1, 128, 128);
+            _emulatorScreenBindings = new List<ScreenBinding>
             {
-                buffer[i] = img[i];
+                emulatorScreen,
+                patternTable0,
+                patternTable1
+            };
+        }
+
+        [MemberNotNull(nameof(_bus))]
+        public void SetBus(Bus bus)
+        {
+            _bus = bus;
+            _bus.Reset();
+        }
+
+        private static void DrawMemoryPage(TextBlock textBlock, ICpu cpu, int address)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int row = 0; row < 16; row++)
+            {
+                sb.Clear();
+                sb.AppendFormat("${0:X4}:", address);
+                for (int col = 0; col < 16; col++)
+                {
+                    sb.AppendFormat(" {0:X2}", cpu.ReadMemory(address));
+                    address++;
+                }
+                string line = sb.ToString();
+                textBlock.Inlines!.Add(new Run(line));
+                textBlock.Inlines!.Add(new LineBreak());
             }
-            return bitmap;
+        }
+
+        private static IImmutableBrush GetFlagBrush(ICpu cpu, Flags6502 flag)
+        {
+            return cpu.GetStatusFlag(flag) ? Brushes.Red : Brushes.Yellow;
+        }
+
+        private static void DrawCpuRegisters(TextBlock textBlock, ICpu cpu)
+        {
+            textBlock.Inlines!.AddRange(
+            [
+                new Run("STATUS: "),
+                new Run(" N") { Foreground = GetFlagBrush(cpu, Flags6502.N) },
+                new Run(" V") { Foreground = GetFlagBrush(cpu, Flags6502.V) },
+                new Run(" -"),
+                new Run(" B") { Foreground = GetFlagBrush(cpu, Flags6502.B) },
+                new Run(" D") { Foreground = GetFlagBrush(cpu, Flags6502.D) },
+                new Run(" I") { Foreground = GetFlagBrush(cpu, Flags6502.I) },
+                new Run(" Z") { Foreground = GetFlagBrush(cpu, Flags6502.Z) },
+                new Run(" C") { Foreground = GetFlagBrush(cpu, Flags6502.C) },
+                new LineBreak(),
+                new Run($"PC: ${cpu.ProgramCounter:X4}"),
+                new LineBreak(),
+                new Run($"A:  ${cpu.AccumulatorRegister:X2}  [{cpu.AccumulatorRegister}]"),
+                new LineBreak(),
+                new Run($"X:  ${cpu.XRegister:X2}  [{cpu.XRegister}]"),
+                new LineBreak(),
+                new Run($"Y:  ${cpu.YRegister:X2}  [{cpu.YRegister}]"),
+                new LineBreak(),
+                new Run($"SP: ${cpu.StackPointer:X4}"),
+            ]);
+        }
+
+        private static void DrawCode(TextBlock textblock, ICpu cpu, int memoryMark, int numberOfLines)
+        {
+
+            var code = Disassemble.FromMemory(cpu, 0x0000, 0xFFFF);
+            var currentMemory = memoryMark - (numberOfLines / 2);
+
+            var splice = code.Where(w => w.Key >= currentMemory)
+                             .Take(numberOfLines)
+                             .ToDictionary(k => k.Key, v => v.Value);
+
+            foreach (var line in splice)
+            {
+                var brush = line.Key == memoryMark ? Brushes.Yellow : Brushes.White;
+                textblock.Inlines!.Add(new Run(line.Value.ToString()) { Foreground = brush });
+                textblock.Inlines!.Add(new LineBreak());
+            }
+        }
+
+        private static void OnUpdate(
+            Bus bus,
+            TextBlock contentBlock,
+            List<ScreenBinding> emulatorScreens,
+            bool drawMemory)
+        {
+            do
+            {
+                bus!.Clock();
+            } while (!bus.Ppu.FrameComplete);
+
+            bus.Ppu.FrameComplete = false;
+
+            foreach (var _emulatorScreenBinding in emulatorScreens)
+                _emulatorScreenBinding.OnUpdate(bus.Ppu);
+
+            contentBlock.Inlines = new InlineCollection();
+
+            if (drawMemory)
+            {
+                DrawMemoryPage(contentBlock, bus.Cpu, 0x0000);
+                contentBlock.Inlines!.Add(new LineBreak());
+                DrawMemoryPage(contentBlock, bus.Cpu, 0x8000);
+            }
+            else
+            {
+                DrawCpuRegisters(contentBlock, bus.Cpu);
+                contentBlock.Inlines!.Add(new LineBreak());
+                DrawCode(contentBlock, bus.Cpu, bus.Cpu.ProgramCounter, 30);
+            }
         }
     }
 }
--- NesUI/MainWindow.axaml.cs
+++ NesUI/MainWindow.axaml.cs
@@ -1,36 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Documents;
 using Avalonia.Interactivity;
-using Avalonia.Media;
-using Avalonia.Media.Imaging;
-using Avalonia.Platform;
-using Avalonia.Threading;
 using Emulator.Components;
-using Emulator.Components.Enums;
-using Emulator.Components.Interfaces;
-using Emulator.Debug;
 
 namespace NesUI;
 
 public partial class MainWindow : Window
 {
-    private Bus? _bus = null!;
-    private DispatcherTimer _timer = new DispatcherTimer();
-    private List<ScreenBinding> _emulatorScreenBindings;
-
-    [MemberNotNull(nameof(_bus))]
-    public void SetBus(Bus bus)
-    {
-        _bus = bus;
-        _bus.Reset();
-    }
-
+    
     public void ClockAction(object? sender, RoutedEventArgs e)
     {
         if (sender is not Button)
@@ -83,138 +60,16 @@ public partial class MainWindow : Window
         InitializeEmulatorWindow();
     }
 
-    private static void DrawMemoryPage(StackPanel panel, ICpu cpu, int address)
-    {
-        StringBuilder sb = new StringBuilder();
-        panel.Children.Clear();
-        for (int row = 0; row < 16; row++)
-        {
-            sb.Clear();
-            sb.AppendFormat("${0:X4}:", address);
-            for (int col = 0; col < 16; col++)
-            {
-                sb.AppendFormat(" {0:X2}", cpu.ReadMemory(address));
-                address++;
-            }
-            string line = sb.ToString();
-            TextBlock txtBlock = new TextBlock
-            {
-                Text = line,
-                Foreground = Brushes.White,
-                FontFamily = "Courier New",
-                FontSize = 14,
-                Height = 16
-            };
-            panel.Children.Add(txtBlock);
-        }
-    }
-
-    private static IImmutableBrush GetFlagBrush(ICpu cpu, Flags6502 flag)
-    {
-        return cpu.GetStatusFlag(flag) ? Brushes.Red : Brushes.Yellow;
-    }
-
-    private static void DrawCpuRegisters(TextBlock textBlock, ICpu cpu)
-    {
-        textBlock.Inlines =
-        [
-            new Run("STATUS: "),
-            new Run(" N") { Foreground = GetFlagBrush(cpu, Flags6502.N) },
-            new Run(" V") { Foreground = GetFlagBrush(cpu, Flags6502.V) },
-            new Run(" -"),
-            new Run(" B") { Foreground = GetFlagBrush(cpu, Flags6502.B) },
-            new Run(" D") { Foreground = GetFlagBrush(cpu, Flags6502.D) },
-            new Run(" I") { Foreground = GetFlagBrush(cpu, Flags6502.I) },
-            new Run(" Z") { Foreground = GetFlagBrush(cpu, Flags6502.Z) },
-            new Run(" C") { Foreground = GetFlagBrush(cpu, Flags6502.C) },
-            new LineBreak(),
-            new Run($"PC: ${cpu.ProgramCounter:X4}"),
-            new LineBreak(),
-            new Run($"A:  ${cpu.AccumulatorRegister:X2}  [{cpu.AccumulatorRegister}]"),
-            new LineBreak(),
-            new Run($"X:  ${cpu.XRegister:X2}  [{cpu.XRegister}]"),
-            new LineBreak(),
-            new Run($"Y:  ${cpu.YRegister:X2}  [{cpu.YRegister}]"),
-            new LineBreak(),
-            new Run($"SP: ${cpu.StackPointer:X4}"),
-        ];
-    }
-    private static void DrawCode(TextBlock textblock, ICpu cpu, int memoryMark, int numberOfLines)
-    {
-
-        var code = Disassemble.FromMemory(cpu, 0x0000, 0xFFFF);
-        var currentMemory = memoryMark - (numberOfLines / 2);
-
-        var splice = code.Where(w => w.Key >= currentMemory)
-                         .Take(numberOfLines)
-                         .ToDictionary(k => k.Key, v => v.Value);
-
-        textblock.Inlines = [new LineBreak()];
-        foreach (var line in splice)
-        {
-            var brush = line.Key == memoryMark ? Brushes.Yellow : Brushes.White;
-            textblock.Inlines.Add(new Run(line.Value.ToString()) { Foreground = brush });
-            textblock.Inlines.Add(new LineBreak());
-        }
-    }
-
-    [MemberNotNull(nameof(_emulatorScreenBindings))]
-    private void InitializeEmulatorWindow()
-    {
-        var emulatorScreen = new ScreenBinding(ScreenSelection.EmulatorScreen, EmulatorWindow, 256, 240);
-        var patternTable0 = new ScreenBinding(ScreenSelection.PatternTable1, PatternTable0, 128, 128);
-        var patternTable1 = new ScreenBinding(ScreenSelection.PatternTable2, PatternTable1, 128, 128);
-        _emulatorScreenBindings = new  List<ScreenBinding>
-        {
-            emulatorScreen,
-            patternTable0,
-            patternTable1
-        };
-    }
-
     public MainWindow(Bus bus)
     : this()
     {
-
         SetBus(bus);
-        //RegistersTextBlock.Text = "";
-        //CodeTextBlock.Text = "";
-
         _timer.Interval = TimeSpan.FromMilliseconds(33);
-
         _timer.Tick += (sender, e) =>
         {
-            do
-            {
-                _bus!.Clock();
-            } while (!_bus.Ppu.FrameComplete);
-
-            _bus.Ppu.FrameComplete = false;
-            
-            foreach (var _emulatorScreenBinding in _emulatorScreenBindings)
-                _emulatorScreenBinding.OnUpdate(_bus.Ppu);
+            OnUpdate(_bus!, EmulatorInformation, _emulatorScreenBindings, _showMemory);
         };
 
         _timer.Start();
-    }
-
-
-
-    private static void DrawEmulation(
-            Bus bus,
-            StackPanel memoryPage0,
-            StackPanel memoryPage1,
-            TextBlock registersTextBlock,
-            TextBlock codeTextBlock,
-            Image image,
-            WriteableBitmap bitmap)
-    {
-        DrawMemoryPage(memoryPage0, bus.Cpu, 0x0000);
-        DrawMemoryPage(memoryPage1, bus.Cpu, 0x8000);
-
-        DrawCpuRegisters(registersTextBlock, bus.Cpu);
-        DrawCode(codeTextBlock, bus.Cpu, bus.Cpu.ProgramCounter, 30);
-        var customBitmap = bus.Ppu.GetScreen();
-        image.Source = UpdateBitmap(bitmap, customBitmap);
     }
 }
